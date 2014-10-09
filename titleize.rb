55ed0caae4c6d37adece016b1c619e8140d393cb
Natalia Bryzhatenko <notika314@gmail.com>
2014-10-08 23:49:01 -0400

    test unit

--- titleize.rb
+++ titleize.rb
@@ -4,7 +4,12 @@ class String
     self.gsub(/(\A|\s)\w/){|letter| letter.upcase}
   end
 end
-raise "F1" unless "this is a test".titleize == "This Is A Test"
-raise "F2" unless "another test 1234".titleize == "Another Test 1234"
-raise "F3" unless "We're testing titleize".titleize =="We're Testing Titleize"
-puts "why don't we all go to the beach this weekend?".titleize
+require 'test/unit'
+
+class TestTitleize < Test::Unit::TestCase
+  def test_basic
+    assert_equal("This Is A Test", "this is a test".titleize)
+    assert_equal("Another Test 1234", "another test 1234".titleize)
+    assert_equal("We're Testing Titleize", "We're testing titleize".titleize)
+  end
+end
